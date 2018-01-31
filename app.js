4568bc237e227cbae1d192fb699065e43d76b4bc
Saavedra <aasaavedra@labs.neit.edu>
2018-01-31 13:00:20 -0500

    Updated files

---
+++ app.js
@@ -0,0 +1,70 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var app = express();
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//var entries = [];
+
+//app.locals.entries = entries;
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", function(req, res){
+	MongoClient.connect(url, function(err,db){
+		if(err)throw err;
+		var dbObj = db.db("games");
+		
+		dbObj.collection("games").find().toArray(function(err, results){
+			console.log("Site Served");
+			db.close();
+			res.render("index",{games:results});
+		});	
+	});
+});
+
+app.get("/new-entry", function(req, res){
+	res.render("new-entry");
+});
+
+app.post("/new-entry", function(req, res){
+	if(!req.body.title || !req.body.body){
+		res.status(400).send("Entries must have some text!");
+		return;
+	}
+	
+	MongoClient.connect(url, function(err, db){
+		if(err) throw err;
+			
+		var dbObj = db.db("games");
+
+		dbObj.collection("games").save(req.body, function(err, results){
+			console.log("Data saved");
+			db.close();
+			res.redirect("/");
+		});		
+	});
+	
+/* 	entries.push({
+		title:req.body.title,
+		content:req.body.body,
+		published:new Date()
+	});	 */
+	//res.redirect("/");
+});
+
+app.use(function(req, res){
+	res.status(404).render("404");
+});
+
+http.createServer(app).listen(3000, function(){
+	console.log("Game library server started on port 3000");
+});
